c3c7c7c5dd97c56a665946bdd9f308a3edced561
Raphaël <Raphael0@users.noreply.github.com>
2016-11-11 22:05:17 +0100

    Update RandomBot.py

--- RandomBot.py
+++ RandomBot.py
@@ -1,14 +1,25 @@
 from hlt import *
 from networking import *
 
-myID, gameMap = getInit()
-sendInit("RandomPythonBot")
+RandomBot, gameMap = getInit()
+sendInit("CyBot")
 
 while True:
     moves = []
     gameMap = getFrame()
     for y in range(gameMap.height):
         for x in range(gameMap.width):
-            if gameMap.getSite(Location(x, y)).owner == myID:
-                moves.append(Move(Location(x, y), int(random.random() * 5)))
+            if gameMap.getSite(Location(x, y)).owner == RandomBot:
+                movedPiece = False
+                for d in CARDINALS:
+                    if gameMap.getSite(Location(x, y), d).owner != RandomBot and gameMap.getSite(Location(x, y), d).strength < gameMap.getSite(Location(x, y)).strength:
+                        moves.append(Move(Location(x, y), d))
+                        movedPiece = True
+                        break
+                if not movedPiece and gameMap.getSite(Location(x, y)).strength < gameMap.getSite(Location(x, y)).production * 5:
+                    moves.append(Move(Location(x, y), STILL))
+                    movedPiece = True
+                if not movedPiece:
+                    moves.append(Move(Location(x, y), NORTH if bool(int(random.random() * 2)) else WEST))
+                    movedPiece = True
     sendFrame(moves)
