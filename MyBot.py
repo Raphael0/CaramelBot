a2ff01a4a898ceab50de5fed04376eefa2b0a1ed
Raphaël <Raphael0@users.noreply.github.com>
2016-11-12 18:13:30 +0100

    Update MyBot.py

--- MyBot.py
+++ MyBot.py
@@ -1,14 +1,111 @@
 from hlt import *
 from networking import *
 
-myID, gameMap = getInit()
-sendInit("MyPythonBot")
+Raphael, gameMap = getInit()
+sendInit("CaramelBot3")
+
+Round = 0
 
 while True:
     moves = []
     gameMap = getFrame()
     for y in range(gameMap.height):
         for x in range(gameMap.width):
-            if gameMap.getSite(Location(x, y)).owner == myID:
-                moves.append(Move(Location(x, y), int(random.random() * 5)))
+            if gameMap.getSite(Location(x, y)).owner == Raphael:
+                
+                Round += 1
+                ready = False
+                attack = False
+                regroup = False
+                move = False
+                reinforce = False
+                flip = 0
+                surrounded = 0
+                for d in CARDINALS:
+                    if gameMap.getSite(Location(x, y), d).owner != Raphael:
+                        surrounded += 1
+                        
+                if not ready and surrounded != 1 and gameMap.getSite(Location(x, y)).strength < 3*gameMap.getSite(Location(x, y)).production:
+                    reinforce = True
+                    ready = True    
+                if not ready and gameMap.getSite(Location(x, y)).strength > 255-gameMap.getSite(Location(x, y)).production:
+                    move = True
+                    ready = True
+                
+                if not ready:
+                    if surrounded == 0:
+                        move = True
+                        ready = True
+                    if surrounded == 1 or surrounded == 2 or surrounded == 4:
+                        attack = True
+                        ready = True
+                    if surrounded == 3:
+                        flip = int(random.random() * 8)
+                        if flip:
+                            attack = True
+                        if not flip:
+                            regroup = True
+                        ready = True
+                
+                        
+                if ready and attack:
+                    for d in CARDINALS:
+                        if gameMap.getSite(Location(x, y), d).owner != Raphael and gameMap.getSite(Location(x, y), d).strength < gameMap.getSite(Location(x, y)).strength:
+                            moves.append(Move(Location(x, y), d))
+                            ready = False
+                        if ready:
+                            reinforce = True
+                if ready and regroup:
+                    for d in CARDINALS:
+                        if gameMap.getSite(Location(x, y), d).owner == Raphael:
+                            moves.append(Move(Location(x, y), d))
+                            ready = False
+                if ready and move:
+                    for i in range(1,10):
+                        if y < i:
+                            if gameMap.getSite(Location(x, y-i+gameMap.height)).owner != Raphael:
+                                moves.append(Move(Location(x, y), NORTH))
+                                ready = False
+                                break
+                        if y > i:
+                            if gameMap.getSite(Location(x, y-i)).owner != Raphael:
+                                moves.append(Move(Location(x, y), NORTH))
+                                ready = False
+                                break
+                        if x > gameMap.width-i:
+                            if gameMap.getSite(Location(x+i-gameMap.width, y)).owner != Raphael:
+                                moves.append(Move(Location(x, y), EAST))
+                                ready = False
+                                break 
+                        if x < gameMap.width-i:
+                            if gameMap.getSite(Location(x+i, y)).owner != Raphael:
+                                moves.append(Move(Location(x, y), EAST))
+                                ready = False
+                                break
+                        if y > gameMap.height-i:
+                            if gameMap.getSite(Location(x, y+i-gameMap.height)).owner != Raphael:
+                                moves.append(Move(Location(x, y), SOUTH))
+                                ready = False
+                                break
+                        if y < gameMap.height-i:
+                            if gameMap.getSite(Location(x, y+i)).owner != Raphael:
+                                moves.append(Move(Location(x, y), SOUTH))
+                                ready = False
+                                break
+                        if x < i:
+                            if gameMap.getSite(Location(x-i+gameMap.width, y)).owner != Raphael:
+                                moves.append(Move(Location(x, y), WEST))
+                                ready = False
+                                break    
+                        if x > i:
+                            if gameMap.getSite(Location(x-i, y)).owner != Raphael:
+                                moves.append(Move(Location(x, y), WEST))
+                                ready = False
+                                break
+                    if ready:
+                        moves.append(Move(Location(x, y), 1+int(random.random() * 2)))
+                        ready = False
+                if ready and reinforce:
+                    moves.append(Move(Location(x, y), STILL))
+                    ready = False
     sendFrame(moves)
